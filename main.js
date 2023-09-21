17f97bde0f0f9b7388d7de04483d712dee7093fe
EMpinzon <EdgarYMauricio@outlook.es>
2023-09-20 23:14:32 -0500

    feat: :sparkles: nuevas funciones para enviar informacion y modificarla

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 let myform = document.querySelector("form")
 let myTabla = document.querySelector("#myData");
+let elementos = document.getElementsByClassName("updateEvent")
 
 
 addEventListener("DOMContentLoaded", async()=>{ 
@@ -7,7 +8,7 @@ addEventListener("DOMContentLoaded", async()=>{
     res.forEach(element => {
         
         myTabla.insertAdjacentHTML("beforeend",`
-            <tr>
+            <tr class ="updateEvent" >
 
                 <td> ${element.id} </td>
                 <td>  ${element.valor} </td>
@@ -28,13 +29,15 @@ addEventListener("DOMContentLoaded", async()=>{
 
 myform.addEventListener("submit",async(e)=>{
     e.preventDefault();
+    
     data = Object.fromEntries( new FormData(e.target))
+    
     const { valor }   = data;
     data.valor = (typeof valor ==="string" ) ? Number(valor) : null;
     let config = {
 
         method: "POST",
-        headers: {"content-type": "application/json"},
+        headers: {"Content-type": "application/json"},
         body: JSON.stringify(data)
     }
     
@@ -43,3 +46,29 @@ myform.addEventListener("submit",async(e)=>{
     console.log(res);
         
 })
+
+console.log(elementos.length)
+
+elementos.forEach( (x)  =>  x.elemento.addEventListener("click",  async(j) => {
+        
+        data = Object.fromEntries( new FormData(j.target))
+    
+        console.log(data);
+
+        let idElemento;
+
+        
+        let config = {
+            method: 'PUT',
+            headers: {
+            'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(),
+        };
+        res = await (await fetch(`https://650b130adfd73d1fab0987b2.mockapi.io/tabla/${idElemento}`,config)).json();
+        
+    }
+))
+
+
+
