87fcf7d76d1986096641c8aa63aac2ed7e3d83a2
EMpinzon <EdgarYMauricio@outlook.es>
2023-09-29 17:28:07 -0500

    refactor: :recycle: Refactorizacion de el codigo

--- main.js
+++ main.js
@@ -7,7 +7,6 @@ let elementos;
 addEventListener("DOMContentLoaded", async()=>{ 
     let res = await ( await fetch("https://650b130adfd73d1fab0987b2.mockapi.io/tabla")).json();
     res.forEach(element => {
-        
         myTabla.insertAdjacentHTML("beforeend",`
             <tr class ="updateEvent" >
 
@@ -16,73 +15,55 @@ addEventListener("DOMContentLoaded", async()=>{
                 <td> ${element.caja} </td>
 
 
-            </tr>
-        
-        
-        `)        
-    }); 
-
-        let modificar = null;
-        elementos =  document.querySelectorAll(".updateEvent  :nth-child(2), .updateEvent   :nth-child(3)");
-        
-        
-        elementos.forEach( (x)  =>  x.addEventListener("click",  async(j) => {
-
-            if(!modificar){
-                let valoraCambiar = (j.target)
-                let nuevoContenido;
-
-                modificar = document.createElement("div");
-                let plantilla ='<input type="text" class="inputValorNuevo" placeholder="Ingrese un nuevo valor">'
-                modificar.innerHTML = plantilla;
-                
-                
-                
-                valoraCambiar.parentNode.parentNode.insertBefore(modificar,valoraCambiar.parentNode.nextSibling)
-                
-                let fila = valoraCambiar.parentNode;
-
-                
-                
-
-                modificar.firstElementChild.addEventListener("keydown",async (e)=>{
-                    nuevoContenido = e.target;
-                    
-                    
-                    if (e.key === 'Enter')  {valoraCambiar.textContent = nuevoContenido.value;  nuevoContenido.parentNode.remove(); modificar=null; 
-                        let config = {
-                            method: 'PUT',
-                            headers: {
-                            'Content-Type': 'application/json',
-                            },
-                            body: JSON.stringify({
-                                "valor": Number(fila.children[1].textContent) ,
-                                "caja": (fila.children[2].textContent ),
-                                "id": fila.children[0].textContent
-                            })
-                        };
-                        let id = Number(fila.children[0].textContent)
-                        console.log(id)
-                        let res2 = await (await fetch(`https://650b130adfd73d1fab0987b2.mockapi.io/tabla/${id}`,config)).json();
-                        console.log(res2)
-                    
-                    }
-                    
-                       
-                })
-
-
-                
-
-
-                
-            }
-            
-                
+            </tr>`
+            )        
         }
-    ))
-        
-
+    ); 
+    let modificar = null;
+    elementos =  document.querySelectorAll(".updateEvent  :nth-child(2), .updateEvent   :nth-child(3)");    
+    elementos.forEach( (x)  =>  x.addEventListener("click",  async(j) => {
+        if(!modificar){
+            let valoraCambiar = (j.target)
+            modificar = document.createElement("div");
+            let plantilla ='<input type="text" class="inputValorNuevo" placeholder="Ingrese un nuevo valor">'
+            modificar.innerHTML = plantilla;
+            valoraCambiar.parentNode.parentNode.insertBefore(modificar,valoraCambiar.parentNode.nextSibling)
+            let fila = valoraCambiar.parentNode;
+            modificar.children[0].addEventListener("keydown",async (e)=>{
+                let nuevoContenido = e.target;
+                if (e.key === 'Enter')  {
+                    valoraCambiar.textContent = nuevoContenido.value;  
+                    nuevoContenido.parentNode.remove(); 
+                    modificar=null; 
+                    let config = {
+                        method: 'PUT',
+                        headers: {
+                        'Content-Type': 'application/json',
+                        },
+                        body: JSON.stringify({
+                            "valor": Number(fila.children[1].textContent) ,
+                            "caja": (fila.children[2].textContent ),
+                            "id": fila.children[0].textContent
+                        })
+                    };
+                    let id = Number(fila.children[0].textContent)
+                    console.log(id)
+                    let res2 = await (await fetch(`https://650b130adfd73d1fab0987b2.mockapi.io/tabla/${id}`,config)).json();
+                    console.log(res2)
+                }
+            }
+            )
+        }
+    }))
+    let eliminar = document.querySelectorAll(".updateEvent  :nth-child(1)");
+    eliminar.forEach(x =>{
+        x.addEventListener('mouseenter',()=>x.style.cursor = "pointer");
+        x.addEventListener('mouseleave',()=>x.style.cursor = "auto");
+        x.addEventListener('click',()=>{
+            x.parentNode.remove();
+            
+        });
+    });
 });
 
 myform.addEventListener("submit",async(e)=>{
