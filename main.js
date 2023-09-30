208ee648bb6398e39d3d2da8d1f0f703b83cb4a7
EMpinzon <EdgarYMauricio@outlook.es>
2023-09-30 00:36:36 -0500

    feat: :sparkles: nueva caracteristica de validar tipo de dato ingresado

--- main.js
+++ main.js
@@ -4,8 +4,8 @@ let elementos;
 
 
 
-addEventListener("DOMContentLoaded", async()=>{ 
-    let res = await ( await fetch("https://650b130adfd73d1fab0987b2.mockapi.io/tabla")).json();
+async function crearTabla (){ 
+    let res = await ( await fetch("http://localhost:3000/registros")).json();
     res.forEach(element => {
         myTabla.insertAdjacentHTML("beforeend",`
             <tr class ="updateEvent" >
@@ -32,6 +32,23 @@ addEventListener("DOMContentLoaded", async()=>{
             modificar.children[0].addEventListener("keydown",async (e)=>{
                 let nuevoContenido = e.target;
                 if (e.key === 'Enter')  {
+                    if(valoraCambiar.parentNode.children[1] === valoraCambiar){
+                        if (isNaN(Number(nuevoContenido.value))){
+                            alert("por favor ingresa un numero");
+                            return ;
+                        }
+                    }else{
+                        let caja = ["ingreso","egreso"];
+                        if (!isNaN(Number(nuevoContenido.value))){
+                            alert("por favor ingresa un string con el tipo de caja");
+                            return ;
+                        }
+                        else if( !caja.includes(nuevoContenido.value.toLowerCase()) ) {
+                            alert("solo puedes ingresar : ingreso y egreso");
+                            return ;
+                        }
+                        
+                    }
                     valoraCambiar.textContent = nuevoContenido.value;  
                     nuevoContenido.parentNode.remove(); 
                     modificar=null; 
@@ -48,7 +65,7 @@ addEventListener("DOMContentLoaded", async()=>{
                     };
                     let id = Number(fila.children[0].textContent)
                     console.log(id)
-                    let res2 = await (await fetch(`https://650b130adfd73d1fab0987b2.mockapi.io/tabla/${id}`,config)).json();
+                    let res2 = await (await fetch(`http://localhost:3000/registros/${id}`,config)).json();
                     console.log(res2)
                 }
             }
@@ -59,34 +76,45 @@ addEventListener("DOMContentLoaded", async()=>{
     eliminar.forEach(x =>{
         x.addEventListener('mouseenter',()=>x.style.cursor = "pointer");
         x.addEventListener('mouseleave',()=>x.style.cursor = "auto");
-        x.addEventListener('click',()=>{
+        x.addEventListener('click',async()=>{
+            let config = {
+                method: "DELETE",
+                headers : {"Content-type": "application/json"},
+            }
+            let id = Number(x.textContent);
+            
+            let res = await (await fetch(`http://localhost:3000/registros/${id}`,config)).json();
             x.parentNode.remove();
+            console.log(res);
             
         });
     });
-});
+};
+
+
 
 myform.addEventListener("submit",async(e)=>{
     e.preventDefault();
-    
-    data = Object.fromEntries( new FormData(e.target))
-    
+    let id = Number(myTabla.lastElementChild.firstElementChild.textContent) + 1;
+    let data = Object.fromEntries( new FormData(e.target))
+    data["id"] = String(id);
     const { valor }   = data;
-    data.valor = (typeof valor ==="string" ) ? Number(valor) : null;
+    data.valor = (typeof valor ==="string" ) ? Number(valor) : valor;
     let config = {
 
         method: "POST",
         headers: {"Content-type": "application/json"},
+        
         body: JSON.stringify(data)
     }
     
-    let res = await (await fetch("https://650b130adfd73d1fab0987b2.mockapi.io/tabla",config)).json();
-    console.log(JSON.stringify(data))
-    console.log(res);
+    let res = await (await fetch("http://localhost:3000/registros",config)).json();
+    
+    
         
 })
 
-
+crearTabla();
 
 
 
